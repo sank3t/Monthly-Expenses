53e98d422e8e3181bc5bd16aa78b391565181cdc
Sanket Sharma <sanketsharma2196@gmail.com>
2023-07-15 23:16:54 +0530

    Added script for table DDLs

---
+++ table_ddl.sql
@@ -0,0 +1,9 @@
+-- Raw table ddl
+CREATE TABLE mnthly_expenses_raw (
+	year_month VARCHAR,
+	month_name VARCHAR,
+	expense_date VARCHAR,
+	expense_category VARCHAR,
+	spent_on VARCHAR,
+	amount VARCHAR
+);
